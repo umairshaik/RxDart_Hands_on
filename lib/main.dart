81f5fabc86cf3eb4091b76a352cf7e27f156970f
Umair Shaik <umair.shaik@thoughtworks.com>
2024-01-24 12:59:31 +0530

    [Umair][002] mock api call and get response via streams

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:rxdart/rxdart.dart';
+import 'package:rxdart_hands_on/views/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const App(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends HookWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // create a behaviour subject every time the widget is re-build
-    final subject = useMemoized(() => BehaviorSubject<String>(), [key]);
-
-    // dispose of the subject every time this widget is disposed or being re-build
-    useEffect(() => subject.close, [subject]);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: StreamBuilder<String>(
-          stream: subject.stream
-              .distinct()
-              .debounceTime(const Duration(seconds: 2)),
-          initialData: 'Please start typing.....',
-          builder: (context, snapshot) {
-            return Text(snapshot.requireData);
-          },
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TextField(
-          onChanged: subject.sink.add,
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
     );
   }
 }
